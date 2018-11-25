e49783277f559de7686c3252ebf8ee11bf15155a
Ashwanth A R <ashwanth1109@gmail.com>
2018-11-25 20:30:57 +0530

    Major issues with passport not picking up model. Retracking to basic server. Come back to user model later

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 //===========================================
 const express = require("express");
 const mongoose = require("mongoose");
-const passport = require("passport");
 const path = require("path");
 const app = express();
 require("dotenv").config();
@@ -28,29 +27,25 @@ mongoose
 //===========================================
 // MIDDLEWARE
 //===========================================
-app.use(express.static(path.join(__dirname, "/../frontend/build"))); // Serving frontend from node server
+app.use(express.static(path.join(__dirname, "/client/build"))); // Serving frontend from node server
 app.use(express.urlencoded({ extended: false })); // For making url encoded postman requests while testing
 app.use(express.json()); // For sending json responses for frontend client
-app.use(passport.initialize()); // Passport middleware for protected routes
-// require("./passport.js")(passport); // Passport config files for implementing jwt strategy
 
 //===========================================
 // REST API ROUTES - CALLED BEFORE CATCH ALL
 // ANY API ROUTE WILL BE CAUGHT FIRST TO RENDER JSON
 // ALL OTHER ROUTES WILL REDIRECT TO FRONTEND INDEX PAGE
 //===========================================
-const usersController = require("./routes/users");
 app.get("/api", (req, res) =>
     res.json({ message: "Hello React. Express is set up properly" })
 );
-app.use("/api/users", usersController);
 
 //===========================================
 // CATCH ALL FOR ANY ROUTE THAT DOESNT EXIST
 // SEND IT TO REACT'S INDEX PAGE
 //===========================================
 app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname + "/../frontend/build/index.html"))
+    res.sendFile(path.join(__dirname + "/../client/build/index.html"))
 );
 
 //===========================================
