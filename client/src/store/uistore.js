04afd480d2bfe671d4fd273bbcd6704f18fc2537
Ashwanth A R <ashwanth1109@gmail.com>
2018-11-26 02:27:40 +0530

    Commented out redux to check if everything else is working on heroku

--- client/src/store/uistore.js
+++ client/src/store/uistore.js
@@ -1,13 +1,13 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
-let ui = function(state = [], action) {
-    switch (action.type) {
-        case "TOGGLE_HEADER":
-            return !action.headerOpen;
-        default:
-            return state;
-    }
-};
+// let ui = function(state = [], action) {
+//     switch (action.type) {
+//         case "TOGGLE_HEADER":
+//             return !action.headerOpen;
+//         default:
+//             return state;
+//     }
+// };
 
-let uistore = createStore(ui);
-export default uistore;
+// let uistore = createStore(ui);
+// export default uistore;
