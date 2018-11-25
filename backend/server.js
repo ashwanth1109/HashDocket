a67be901255bf86327bc26e7d001dceb0a5b018e
Ashwanth A R <ashwanth1109@gmail.com>
2018-11-25 16:53:11 +0530

    Succesfully connected to MONGODB on localhost. Checking on heroku now

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,44 @@
+//===========================================
+// IMPORT DEPENDENCIES
+//===========================================
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
 const path = require("path");
+const app = express();
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3001;
+//===========================================
+// PORT & MONGODB URI
+//===========================================
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-app.use(express.static(path.join(__dirname, "/../frontend/build")));
-app.use(express.json());
+//===========================================
+// SET UP MONGO DB CONNECTION TO MLAB URI
+//===========================================
+mongoose
+    .connect(
+        MONGODB_URI,
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log(`MONGODB connection established succesfully`))
+    .catch(err => console.log(err));
 
+//===========================================
+// MIDDLEWARE
+//===========================================
+app.use(express.static(path.join(__dirname, "/../frontend/build"))); // Serving frontend from node server
+app.use(express.urlencoded({ extended: false })); // For making url encoded postman requests while testing
+app.use(express.json()); // For sending json responses for frontend client
+app.use(passport.initialize()); // Passport middleware for protected routes
+// require("./passport.js")(passport); // Passport config files for implementing jwt strategy
+
+//===========================================
+// REST API ROUTES - CALLED BEFORE CATCH ALL
+// ANY API ROUTE WILL BE CAUGHT FIRST TO RENDER JSON
+// ALL OTHER ROUTES WILL REDIRECT TO FRONTEND INDEX PAGE
+//===========================================
 app.get("/api", (req, res) => res.json({ message: "Hello React" }));
 
 //===========================================
@@ -17,4 +49,7 @@ app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname + "/../frontend/build/index.html"))
 );
 
+//===========================================
+// SET UP APP LISTENER ON PORT
+//===========================================
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
