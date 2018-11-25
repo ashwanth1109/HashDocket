631f6c1f2290e2b6c975fd646d38e758598179f4
Ashwanth A R <ashwanth1109@gmail.com>
2018-11-25 09:26:20 +0530

    last change before break. This time for real

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const PORT = process.env.PORT1 || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "/../frontend/build")));
 app.use(express.json());
