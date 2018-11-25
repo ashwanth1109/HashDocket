26bb5a57d807955dc8ec5ac022ad5b33b3f42d1e
Ashwanth A R <ashwanth1109@gmail.com>
2018-11-25 09:23:23 +0530

    last change before break. This time for real

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT1 || 3001;
 
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "/../frontend/build")));
 app.use(express.json());
 
 app.get("/api", (req, res) => res.json({ message: "Hello React" }));
---
+++ frontend/build/precache-manifest.ec3e5fb27f65bd7e505048efd4f17bb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e33a327d495b7a641091",
+    "url": "/static/js/main.e33a327d.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "e33a327d495b7a641091",
+    "url": "/static/css/main.efa2dca0.chunk.css"
+  },
+  {
+    "revision": "09e5e3a6fe14daf330dec1ef30722f95",
+    "url": "/index.html"
+  }
+];
