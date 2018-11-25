1964c04e3d5e4c53fedbf41dac593c70a97bef9c
Ashwanth A R <ashwanth1109@gmail.com>
2018-11-25 08:19:46 +0530

    more heroku changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3001;
+
 app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/api", (req, res) => res.json({ message: "Hello React" }));
 
-app.listen(3001, () => console.log(`listening on port 3001`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
